d53b4c2a90f693c9c94af92f6edc79affca26ae3
ilvro <cildumyt@gmail.com>
2024-07-23 20:18:01 -0300

    upload popup styling

--- upload-song.js
+++ upload-song.js
@@ -2,13 +2,24 @@ const uploadSongBtn = document.getElementById('upload-song-btn');
 const uploadPopup = document.getElementById('upload-popup');
 const uploadSubmit = document.getElementById('upload-submit');
 const uploadCancel = document.getElementById('upload-cancel')
+const dimmer = document.getElementById('dimmer');
 
 uploadSongBtn.addEventListener('click', () => {
-    uploadPopup.style.display = 'block';
+    uploadPopup.style.visibility = 'visible';
+    dimmer.style.visibility = 'visible';
+    setTimeout(() => {
+        uploadPopup.style.opacity = '1';
+        dimmer.style.opacity = '0.6';
+    }, 10);
 });
 
 uploadCancel.addEventListener('click', () => {
-    uploadPopup.style.display = 'none';
+    uploadPopup.style.opacity = '0';
+    dimmer.style.opacity = '0';
+    setTimeout(() => {
+       uploadPopup.style.visibility = 'hidden';
+       dimmer.style.visibility = 'hidden'; 
+    }, 10);
 });
 
 uploadSubmit.addEventListener('click', () => {
