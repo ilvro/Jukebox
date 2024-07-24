0c1ef795b590f74b6539bec1629f9f1a084e0708
ilvro <cildumyt@gmail.com>
2024-07-24 11:53:00 -0300

    added a youtube link input bar for downloading music - working on the backend

--- upload-song.js
+++ upload-song.js
@@ -1,8 +1,19 @@
 const uploadSongBtn = document.getElementById('upload-song-btn');
 const uploadPopup = document.getElementById('upload-popup');
 const uploadSubmit = document.getElementById('upload-submit');
-const uploadCancel = document.getElementById('upload-cancel')
+const uploadCancel = document.getElementById('upload-cancel');
+const youtubeLinkInput = document.getElementById('youtube-link');
 const dimmer = document.getElementById('dimmer');
+
+youtubeLinkInput.addEventListener('paste', (event) => {
+    let youtubeLink = event.clipboardData.getData("text");
+    if (youtubeLink.includes("youtube.com/watch?v=")) {
+        // server should handle this. its downloading a video and converting it to mp3, probably using the yt-dlp library
+    }
+    else {
+        console.log("invalid url")
+    }
+});
 
 uploadSongBtn.addEventListener('click', () => {
     uploadPopup.style.visibility = 'visible';
