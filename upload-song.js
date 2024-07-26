5e2d47f784569ca20b12098bda81e2ea69576d07
ilvro <cildumyt@gmail.com>
2024-07-26 12:42:43 -0300

    echo test for the server

--- upload-song.js
+++ upload-song.js
@@ -8,13 +8,32 @@ const dimmer = document.getElementById('dimmer');
 youtubeLinkInput.addEventListener('paste', (event) => {
     let youtubeLink = event.clipboardData.getData("text");
     if (youtubeLink.includes("youtube.com/watch?v=")) {
-        // server should handle this. its downloading a video and converting it to mp3, probably using the yt-dlp library
+        // server should handle this
+        downloadVideo(youtubeLink);
     }
     else {
         console.log("invalid url")
     }
 });
 
+function downloadVideo(youtubeLink) {
+    fetch('http://localhost:3000/echo', { // will change this url later when the server is deployed
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({message: youtubeLink})
+      })
+        .then(response => response.text())
+        .then(data => console.log(data));      
+}
+
+
+uploadSubmit.addEventListener('click', () => {
+
+});
+
+// ------------------- styling ------------------------------
 uploadSongBtn.addEventListener('click', () => {
     uploadPopup.style.visibility = 'visible';
     dimmer.style.visibility = 'visible';
@@ -31,8 +50,4 @@ uploadCancel.addEventListener('click', () => {
        uploadPopup.style.visibility = 'hidden';
        dimmer.style.visibility = 'hidden'; 
     }, 10);
-});
-
-uploadSubmit.addEventListener('click', () => {
-
 });
