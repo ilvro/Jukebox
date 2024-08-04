b92dc4d61bc09c6cce790186efbb74f64f45d6ac
ilvro <cildumyt@gmail.com>
2024-08-04 10:53:07 -0300

    added search bar functionality

--- filters.js
+++ filters.js
@@ -15,6 +15,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+function searchInput() {
+    let searchQuery = document.getElementById('searchBar').value.toLowerCase();
+    const songs = document.querySelectorAll('.song-item');
+
+
+    for (let i = 0; i < songs.length; i++) {
+        if (songs[i].querySelector('h3').textContent.toLowerCase().includes(searchQuery.toLowerCase())) {
+            songs[i].style.display = 'block';
+        }
+        else {
+            songs[i].style.display = 'none';
+        }
+    }
+}
+
 function filterSongs(genre) {
     const songs = document.querySelectorAll('.song-item');
     songs.forEach(song => {
